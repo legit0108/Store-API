c85148b8e19647e3bd12b63b555280124b00890b
legit0108 <legitgamer2345@gmail.com>
2022-08-03 17:01:55 +0530

    added products to db and implemented filtering controller

--- controllers/products.js
+++ controllers/products.js
@@ -1,5 +1,9 @@
+const Product = require('../models/product')
+
+// hardcoding name , other property values 
 const getAllProductsStatic = async(req,res) =>{
- res.status(200).send({msg:'products testing route'})
+ const products = await Product.find({})
+ res.status(200).send({products,totalHits:products.length})
 }
 
 const getAllProducts = async(req,res) =>{
